c6278d2336bb6af8a65fb25476a52623db971906
Djenan Kozic <djenan@mac.com>
2014-02-11 12:19:52 +0000

    Fleshed out and tidied up entire SASS file package

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@ require 'susy'
 require 'rgbapng'
 require './sass/svg/svg.rb'
 
+
 # Set this to the root of your project when deployed:
 http_path = "../"
 css_dir = "css"
@@ -11,12 +12,15 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compact
+output_style = :expanded
+
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
@@ -27,6 +31,4 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
-
-sass_options = { :debug_info => false }
+sass_options = { :debug_info => true }
